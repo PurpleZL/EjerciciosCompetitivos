2d14d9d15037280a19d7e0b134ed5e597e202f5d
PurpleZL <vladutorza64@gmail.com>
2025-06-30 17:29:47 +0200

    .

---
+++ src/CodeWars/ComplementaryDNA.java
@@ -0,0 +1,22 @@
+package CodeWars;
+
+import java.util.stream.Collectors;
+
+public class ComplementaryDNA {
+    public static String makeComplement(String dna) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dna.length(); i++) {
+            char c = dna.charAt(i);
+            if (c == 'T') {
+                sb.append('A');
+            } else if (c == 'A') {
+                sb.append('T');
+            } else if (c == 'C') {
+                sb.append('G');
+            } else if (c == 'G') {
+                sb.append('C');
+            }
+        }
+        return sb.toString();
+    }
+}

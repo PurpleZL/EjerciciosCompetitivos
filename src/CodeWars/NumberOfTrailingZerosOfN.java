dffbe2124edcd1574a395f060588cb97e4f64630
PurpleZL <vladutorza64@gmail.com>
2025-08-12 09:30:48 +0200

    .

---
+++ src/CodeWars/NumberOfTrailingZerosOfN.java
@@ -0,0 +1,11 @@
+package CodeWars;
+
+public class NumberOfTrailingZerosOfN {
+    public static int zeros(int n) {
+        int count = 0;
+        for (int i = 5; n / i >= 1; i *= 5) {
+            count += n / i;
+        }
+        return count;
+    }
+}
